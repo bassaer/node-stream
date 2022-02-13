780f2e82bfdb9d7a1f8969f881a447d7f13f236b
bassaer <app.nakayama@gmail.com>
2022-02-13 18:16:39 +0900

    add sample

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("OK");
+import fs from 'fs';
+import http from 'http';
+import net from 'net';
+
+const src = fs.createReadStream('./data/in.txt', 'utf8');
+const dst = fs.createWriteStream('./data/out.txt', 'utf8');
+
+src.pipe(dst);
+
+const server = http.createServer((req, res) => {
+    res.end("OK");
+});
+
+const port = 3000;
+server.listen(port, () => {
+    console.log(`start: http://localhost:${port}`);
+
+    const res = fs.createWriteStream('./data/res.txt', 'utf8');
+    const client = net.connect(port, 'localhost');
+    client.pipe(res);
+    client.once('connect', () => client.write('GET / HTTP/1.1\r\n\r\n'));
+    client.on('close', () => server.close());
+});
